ca892afd386062877ba212c4de496255be8de8f5
julio <juliovilar93@gmail.com>
2021-03-16 10:57:11 -0300

    chore: melhorando componentizacao e reuso das interfaces

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,31 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+import { IMovie } from "../util/interfaces/movie.interface";
+interface IPROPS {
+  title: string;
+  movies: IMovie[];
+}
+
+export function Content(props: IPROPS) {
+  return (
+    <>
+      <header>
+        <span className="category">
+          Categoria:<span> {props.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+import { IGenre } from "../util/interfaces/genre.interface";
+
+interface IPROPS {
+  genres: IGenre[];
+  handleClickButton: Function;
+  selectedGenreId: Number;
+}
+
+export function SideBar(props: IPROPS) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
